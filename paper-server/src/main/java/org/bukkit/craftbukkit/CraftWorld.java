1558a0ecc924a26e9e4fd7eb75d1fb1191b9ae00
granny <contact@granny.dev>
2025-04-02 19:53:49 -0700

    more vomit

--- paper-server/src/main/java/org/bukkit/craftbukkit/CraftWorld.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/CraftWorld.java
@@ -1288,7 +1288,7 @@ public class CraftWorld extends CraftRegionAccessor implements World {
 
     @Override
     public void setDifficulty(Difficulty difficulty) {
-        this.getHandle().theGame().server().setDifficulty(this.getHandle(), net.minecraft.world.Difficulty.byId(difficulty.getValue()), true); // Paper - per level difficulty; don't skip other difficulty-changing logic
+        this.getHandle().theGame().setDifficulty(this.getHandle(), net.minecraft.world.Difficulty.byId(difficulty.getValue()), true); // Paper - per level difficulty; don't skip other difficulty-changing logic
     }
 
     @Override
